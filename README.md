465b6c693d37fdf22824c87bf32f5d3a984c10f1
henrykironde <henrykironde@gmail.com>
2017-10-04 19:32:27 -0400

    Working on get opts this should accept weav install sqlite config path-to-my-conf

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# weav
-test
+# weaver
+
+
+[![Build Status](https://travis-ci.org/henrykironde/weaverhenry.svg?branch=master)](https://travis-ci.org/henrykironde/weaverhenry)
+[![Documentation Status](https://readthedocs.org/projects/weaverhenry/badge/?version=latest)](http://weaverhenry.readthedocs.org/en/latest/?badge=latest)
+[![License](http://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/weecology/retriever/master/LICENSE)
+
+The Data weaver offers robust data integration of data sets.
+Data sets are always stored in various forms and these could be Spacial data in raster or vector forms or tabular data (numbers and text) in plain text like csv, tsv, txt
+ 
+The current trend of science involves and requires studying entities with abroad range of associations. 
+These associations to the given entity understudy could be achieved through collecting and integrating various sources and forms of data.
+The data weaver handles the finding and integration of the data and installing  and hence providing a unified platform to integrate heterogeneous data sources on different data storage systems 
+ 
+
+Dependencies
+------------
+
+This package depends on the following packages:
+
+     xlrd>=0.7
+     PyMySQL>=0.4
+     psycopg2>=2.0
+     sqlalchemy
+     gdal
+
+They can be installed using ``pip``.
+
+    sudo pip install -r requirements.txt
+
+
+
+Installation
+------------
+
+To install, run the following command from the top-level package
+directory.
+ 
+
+    sudo python setup.py install
+
+Getting Started
+---------------
+
+The .....
+
+
+
+Contribution
+------------
+
+If you find any operation that is not supported by this package, feel
+free to create a Github issue. Also, you are more than welcome to submit
+a pull request for a bug fix or additional feature.
+
+Developer's Guide
+--------------
+
+
+If you want to contribute to this package, you will need the ``pytest``
+package as well.
+
+ 
+Acknowledgment
+--------------
+
+The Development of this software is funded by Ethan White .............
---
+++ weav/__main__.py
@@ -0,0 +1,68 @@
+"""Data Retriever Wizard
+
+Running this module directly will launch the download wizard, allowing the user
+to choose from all scripts.
+
+The main() function can be used for bootstrapping.
+
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+from builtins import input
+from imp import reload
+
+from weav.engines import engine_list, choose_engine
+from weav.lib.datapackage import create_json, edit_json, delete_json, get_script_filename
+from weav.lib.datasets import datasets, license
+from weav.lib.defaults import sample_script, CITATION, ENCODING
+from weav.lib.get_opts import parser
+from weav.lib.repository import check_for_updates
+from weav.lib.scripts import SCRIPT_LIST
+from weav.lib.tools import name_matches, reset_retriever
+
+encoding = ENCODING.lower()
+# sys removes the setdefaultencoding method at startup; reload to get it back
+reload(sys)
+if hasattr(sys, 'setdefaultencoding'):
+    sys.setdefaultencoding(encoding)
+
+
+def main():
+    """This function launches the weav."""
+    sys.argv[1:] = [arg.lower() for arg in sys.argv[1:]]
+    if len(sys.argv) == 1:
+        # if no command line args are passed, show the help options
+        parser.parse_args(['-h'])
+
+    else:
+
+        args = parser.parse_args()
+
+        if args.quiet:
+            sys.stdout = open(os.devnull, 'w')
+
+        if args.command == 'help':
+            parser.parse_args(['-h'])
+
+        # Citations and Ls are going to import from retriever
+
+        if args.command == 'citation':
+            if args.dataset is None:
+                # get the citation of weaver
+                print("ToDos::citation of weaver")
+                return
+            else:
+                print("ToDos::the citation for dataset "+args.dataset )
+
+        # list the data sets available
+        if args.command == 'ls':
+            print("ToDos:: list of all available data sets.\n "
+                  "Either we search the default retriever data folder")
+            return
+
+
+if __name__ == "__main__":
+    main()
