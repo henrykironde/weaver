eb519c4aeaefb105511003d50b6261a6e9ba8d0a
henrykironde <henrykironde@gmail.com>
2018-10-08 18:50:58 -0400

    almost

--- weaver/lib/process.py
+++ weaver/lib/process.py
@@ -1,22 +1,29 @@
 from __future__ import print_function
 
-
+from collections import OrderedDict
 # from weaver.lib.models import *
 
 
 class Processor(object):
     def make_sql(dataset):
-        processed_as = {}
-        processed_as[dataset.main_file["path"]] = "t1"
+        # 1. For spatial joins,
+        # assume that the latitude and longitude fields are in the main file.
+        #
+        # use ST_PointFromText ST_PointFromText('POINT(-71.064 42.28)', 4326);
+
+        main_table_path = dataset.main_file["path"]
+        as_table = OrderedDict([(main_table_path, "T1")])
+        as_table[dataset.main_file["path"]] = "t1"
         main_sql_join = ""
         all_fields = []
-        string_field = ""
+        all_field_string = ""
         make_temp = False
 
+        # Process all tables that are to be joined and the fields that are required
         for counter, tabletojoin in enumerate(dataset.join):
             as_tables = "as_" + str(counter)
             dot_tablevalue = as_tables + "."
-            processed_as[tabletojoin["table"]] = as_tables
+            as_table[tabletojoin["table"]] = as_tables
 
             f_fields_used = []
             if "fields_to_use" in tabletojoin:
@@ -53,8 +60,8 @@ class Processor(object):
                     if tabletojoin["join_ocn"]["common_field"]:
                         for item_field in tabletojoin["join_ocn"]["common_field"]:
                             string_b4_list += ["{t11}.{samefield}={t22}.{samefield}".format(
-                                t11=processed_as[dataset.main_file["path"]],
-                                t22=processed_as[tabletojoin["table"]],
+                                t11=as_table[dataset.main_file["path"]],
+                                t22=as_table[tabletojoin["table"]],
                                 samefield=str(item_field))]
                     # process non common fields
                     temp_dict = tabletojoin["join_ocn"]
@@ -65,37 +72,37 @@ class Processor(object):
                     string_3_on_list = []
                     for counter, items in enumerate(temp_dict[lll[0]]):
                         new_on = "{tt1}.{f11}={tt2}.{f22}".format(
-                            tt1=processed_as[str(lll[0])],
+                            tt1=as_table[str(lll[0])],
                             f11=items,
-                            tt2=processed_as[str(lll[1])],
+                            tt2=as_table[str(lll[1])],
                             f22=temp_dict[lll[0]][counter]
                         )
                         string_3_on_list.append(new_on)
                     str_2_j_on = "\nAND \n".join(str(e) for e in string_3_on_list + string_b4_list)
                     left_join += str_2_j_on
                     main_sql_join += left_join
-        #
-        # # Process the main file and create the query string for all the required fields
-        # if "fields" in dataset.main_file:
-        #     if not dataset.main_file["fields"]:
-        #         bmain_sql_join = "\nSELECT * \nFROM {main_table}  AS  t1 " \
-        #                          "".format(main_table=dataset.main_file["path"])
-        #     else:
-        #         all_fields = dataset.main_file["fields"]
-        #         all_fields = ["t1." + itemkk for itemkk in dataset.main_file["fields"]]
-        #         bmain_sql_join = "\nSELECT {all_fls} \nFROM {main_table} AS t1 " \
-        #                          "".format(all_fls="{all_flds}", main_table=dataset.main_file["path"])
-        #         if make_temp:
-        #             # ["latitude", "longitude"]
-        #             y = dataset.main_file["lat_long"][0]
-        #             x = dataset.main_file["lat_long"][1]
-        #
-        #             temp_fields = ["temp." + itemkk for itemkk in dataset.main_file["fields"]]
-        #             temp_fields_string = ', '.join(str(e) for e in temp_fields) + ", "
-        #             temp_geom_value = temp_fields_string + " (ST_SetSRID(ST_MakePoint(cast(temp.{x1} as Numeric(10, 4)), cast(temp.{y1} as Numeric(10, 4))), 4326)) as the_geom".format(
-        #                 x1=x, y1=y)
-        #             bmain_sql_join = "\nSELECT * FROM (SELECT " + temp_geom_value + " FROM {main_table} temp) t1"
-        #
-        # str_allfields = ', '.join(str(e) for e in all_fields)
-        #
-        # return bmain_sql_join +" "+ main_sql_join.format(all_flds=str_allfields)
+
+        # Process the main file and create the query string for all the required fields
+        if "fields" in dataset.main_file:
+            if not dataset.main_file["fields"]:
+                bmain_sql_join = "\nSELECT * \nFROM {main_table}  AS  t1 " \
+                                 "".format(main_table=dataset.main_file["path"])
+            else:
+                all_fields = dataset.main_file["fields"]
+                all_fields = ["t1." + itemkk for itemkk in dataset.main_file["fields"]]
+                bmain_sql_join = "\nSELECT {all_fls} \nFROM {main_table} AS t1 " \
+                                 "".format(all_fls="{all_flds}", main_table=dataset.main_file["path"])
+                if make_temp:
+                    # ["latitude", "longitude"]
+                    y = dataset.main_file["lat_long"][0]
+                    x = dataset.main_file["lat_long"][1]
+
+                    temp_fields = ["temp." + itemkk for itemkk in dataset.main_file["fields"]]
+                    temp_fields_string = ', '.join(str(e) for e in temp_fields) + ", "
+                    temp_geom_value = temp_fields_string + " (ST_SetSRID(ST_MakePoint(cast(temp.{x1} as Numeric(10, 4)), cast(temp.{y1} as Numeric(10, 4))), 4326)) as the_geom".format(
+                        x1=x, y1=y)
+                    bmain_sql_join = "\nSELECT * FROM (SELECT " + temp_geom_value + " FROM {main_table} temp) t1"
+
+        str_allfields = ', '.join(str(e) for e in all_fields)
+
+        return bmain_sql_join +" "+ main_sql_join.format(all_flds=str_allfields)
